cb5e4d451baf7d5dd61030e6267cf031613a94f6
AleksandrMo <sushanta@mail.ru>
2022-01-24 16:02:17 +0700

    commit

--- src/main/java/ru/servlet/CandidateServlet.java
+++ src/main/java/ru/servlet/CandidateServlet.java
@@ -1,7 +1,6 @@
 package ru.servlet;
 
 import ru.model.Candidate;
-import ru.model.Post;
 import ru.store.Store;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +14,7 @@ import java.io.IOException;
         @Override
         protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             req.setCharacterEncoding("UTF-8");
-            Store.instOf().save(new Candidate(0, req.getParameter("name")));
+            Store.instOf().save(new Candidate(Integer.valueOf(req.getParameter("id")),  req.getParameter("name")));
             resp.sendRedirect(req.getContextPath() + "/candidates.jsp");
         }
     }
--- src/main/java/ru/servlet/PostServlet.java
+++ src/main/java/ru/servlet/PostServlet.java
@@ -8,13 +8,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PostServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
-        Store.instOf().save(new Post(0, req.getParameter("name"), req.getParameter("description")));
+        Store.instOf().save(
+                new Post(
+                        Integer.valueOf(req.getParameter("id")),
+                        req.getParameter("name"), ""
+                )
+        );
         resp.sendRedirect(req.getContextPath() + "/posts.jsp");
     }
 
